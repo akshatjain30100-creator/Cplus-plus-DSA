681fa11b4f1d04a5838558b40ca362e581a7a09c
akshatjain30100-creator <akshatjain30100@gmail.com>
2026-01-17 17:19:46 +0530

    Update Armstrong.cpp

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -3,5 +3,14 @@
 using namespaces as std 
 int main()
 {
-       int num,rom,cube
+       int num,rom,cube,sum=0,digit;
+       cout>>"enter a no"
+       cin<<num;
+       rom=num;
+       while(num!=0)
+       {
+         digit = num%10;
+         cube = digit * digit * digit; 
+          sum=
+       }
 }
